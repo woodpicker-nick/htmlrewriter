c4e1a31b646e053c93233615412e7a5883b86350
remorses <beats.by.morse@gmail.com>
2023-07-27 15:57:39 +0200

    types

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import init, { HTMLRewriter as RawHTMLRewriter } from './dist/html_rewriter'
+import init, { HTMLRewriter as RawHTMLRewriter } from '../dist/html_rewriter'
 // @ts-ignore
 import wasm from './dist/html_rewriter_bg.wasm?module'
+import { DocumentHandlers, ElementHandlers } from './types'
 const encoder = new TextEncoder()
 const decoder = new TextDecoder()
 
-type ElementHandlers = {
-    element?(element: any): void | Promise<void>
-}
+
+
 
 // console.log(wasm)
 let p = init(wasm)
 export class HTMLRewriter {
-    private elementHandlers: [selector: string, handlers: any][] = []
+    private elementHandlers: [selector: string, handlers: ElementHandlers][] = []
+    private documentHandlers: DocumentHandlers[] = []
 
     constructor(private readonly options?: any) {}
 
     on(selector: string, handlers: ElementHandlers): this {
         this.elementHandlers.push([selector, handlers])
+        return this
+    }
+
+    onDocument(handlers: DocumentHandlers): this {
+        this.documentHandlers.push(handlers)
         return this
     }
 
@@ -28,6 +34,9 @@ export class HTMLRewriter {
         }, this.options)
         for (const [selector, handlers] of this.elementHandlers) {
             rewriter.on(selector, handlers)
+        }
+        for (const handlers of this.documentHandlers) {
+            rewriter.onDocument(handlers)
         }
 
         try {
