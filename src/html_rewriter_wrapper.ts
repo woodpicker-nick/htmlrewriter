329b02906036bf90ecf51c845dabe4857a94d80c
remorses <beats.by.morse@gmail.com>
2023-08-02 13:59:54 +0200

    nn

--- src/html_rewriter_wrapper.ts
+++ src/html_rewriter_wrapper.ts
@@ -27,27 +27,59 @@ export function HTMLRewriterWrapper(initPromise: Promise<any>) {
             this.documentHandlers.push(handlers)
             return this
         }
+        transform(response: Response): Response {
+            const body = response.body as ReadableStream<Uint8Array> | null
+            // HTMLRewriter doesn't run the end handler if the body is null, so it's
+            // pointless to setup the transform stream.
+            if (body === null) return new Response(body, response)
 
-        async transform(res: Response) {
-            await this.initPromise
-            let output = ''
-            const rewriter = new RawHTMLRewriter((chunk: any) => {
-                output += decoder.decode(chunk)
-            }, this.options)
-            for (const [selector, handlers] of this.elementHandlers) {
-                rewriter.on(selector, handlers)
-            }
-            for (const handlers of this.documentHandlers) {
-                rewriter.onDocument(handlers)
+            if (response instanceof Response) {
+                // Make sure we validate chunks are BufferSources and convert them to
+                // Uint8Arrays as required by the Rust glue code.
+                response = new Response(response.body, response)
             }
 
-            try {
-                await rewriter.write(encoder.encode(await res.text()))
-                await rewriter.end()
-                return new Response(output, res)
-            } finally {
-                rewriter.free()
-            }
+            let rewriter: RawHTMLRewriter
+            const transformStream = new TransformStream<Uint8Array, Uint8Array>(
+                {
+                    start: async (controller) => {
+                        // Create a rewriter instance for this transformation that writes its
+                        // output to the transformed response's stream. Note that each
+                        // BaseHTMLRewriter can only be used once.
+                        await this.initPromise
+                        rewriter = new RawHTMLRewriter(
+                            (chunk: any) => {
+                                // enqueue will throw on empty chunks
+                                if (chunk.length !== 0)
+                                    controller.enqueue(chunk)
+                            },
+                            { enableEsiTags: false },
+                        )
+                        // Add all registered handlers
+                        for (const [selector, handlers] of this
+                            .elementHandlers) {
+                            rewriter.on(selector, handlers)
+                        }
+                        for (const handlers of this.documentHandlers) {
+                            rewriter.onDocument(handlers)
+                        }
+                    },
+                    // The finally() below will ensure the rewriter is always freed.
+                    // chunk is guaranteed to be a Uint8Array as we're using the
+                    // @miniflare/core Response class, which transforms to a byte stream.
+                    transform: (chunk) => rewriter.write(chunk),
+                    flush: () => rewriter.end(),
+                },
+            )
+            const promise = body.pipeTo(transformStream.writable)
+            promise.catch(() => {}).finally(() => rewriter?.free())
+
+            // Return a response with the transformed body, copying over headers, etc
+            const res = new Response(transformStream.readable, response)
+            // If Content-Length is set, it's probably going to be wrong, since we're
+            // rewriting content, so remove it
+            res.headers.delete('Content-Length')
+            return res
         }
     }
 }
